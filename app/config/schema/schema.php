a826b4ef9397e58c9f03cfbc05b9f48eef3c4c41
Jonathan Dalrymple <jonathan@Blackbook.local>
2010-08-14 15:14:32 +0100

    Added the config

---
+++ app/config/schema/schema.php
@@ -0,0 +1,64 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* App schema generated on: 2010-08-13 17:08:58 : 1281718318*/
+class AppSchema extends CakeSchema {
+	var $name = 'App';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $issues = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'description' => array('type' => 'text', 'null' => true, 'default' => NULL),
+		'status_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'project_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'version_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'creator_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'assigned_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $projects = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $statuses = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'weighting' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'password' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 64),
+		'email' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+	var $versions = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'project_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'commit' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 64),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'MyISAM')
+	);
+}
+?>
